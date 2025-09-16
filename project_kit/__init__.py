96c377c8c381ca99bf7e6949b36c432c3bf0050b
brookie guzder-williams <bguzder-williams@berkeley.edu>
2025-09-16 12:03:21 -0700

    project_kit/__init__: doc-string

--- project_kit/__init__.py
+++ project_kit/__init__.py
@@ -1,1 +1,8 @@
-# __init__.py
+"""
+Project Kit Package
+
+A comprehensive collection of tools for building Python projects with
+sophisticated configuration management and job execution capabilities.
+
+License: CC-BY-4.0
+"""
