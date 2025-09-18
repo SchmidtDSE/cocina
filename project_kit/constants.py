49e00fb2bad180b267325abed7972db5f43c6772
brookie guzder-williams <bguzder-williams@berkeley.edu>
2025-09-18 14:30:29 -0700

    constants: icons

--- project_kit/constants.py
+++ project_kit/constants.py
@@ -10,10 +10,19 @@ License: CC-BY-4.0
 #
 # CONSTANTS
 #
-# pkit
+
+# pkit:core
 PKIT_CONFIG_FILENAME: str = '.pkit'
 PKIT_NOT_FOUND: str = '__PKIT_OBJECT_NOT_FOUND'
+
+# pkit:cli
+PKIT_CLI_DEFAULT_HEADER = 'pkit_job'
 
 # filenames/paths/etc
 PY_EXT_REGX: str = r'\.py$'
 YAML_EXT_REGX = r'\.(yaml|yml)$'
+
+# icons
+ICON_START = "🚀"
+ICON_FAILED = "❌"
+ICON_SUCCESS = "✅"
